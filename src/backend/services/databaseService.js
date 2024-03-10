498ba01274c87e295b5bd11ace0c60182360f22c
jamesedra <dedralin@students.cs.ubc.ca>
2024-03-10 00:47:48 -0800

    db service tweaks

--- src/backend/services/databaseService.js
+++ src/backend/services/databaseService.js
@@ -17,21 +17,47 @@ const dbConfig = {
 // Create a new PostgreSQL client
 const pool = new Pool(dbConfig);
 
-(async () => {
+pool.connect(async (err, client, release) => {
+  if (err) {
+    console.error("Error connecting to PostgreSQL server: ", err.message);
+    return;
+  }
+  console.log("Pool connected.");
   try {
-    const { rows } = await pool.query("SELECT current_user");
+    const { rows } = await client.query("SELECT current_user");
     const currentUser = rows[0]["current_user"];
     console.log("Current user:", currentUser);
   } catch (err) {
     console.error("Error executing query", err);
+  } finally {
+    release();
   }
-})();
-
-// Close the pool when the Node.js process terminates
-process.on("SIGINT", () => {
-  pool.end();
-  console.log("Pool Terminated");
-  process.exit();
 });
 
+function closePoolAndExit() {
+  console.log("\nTerminating pool...");
+  pool.end((err) => {
+    if (err) {
+      console.error("Error on closing pool: ", err.message);
+      process.exit(1);
+    }
+    console.log("Pool closed.");
+    process.exit(0);
+  });
+}
+
+// Close the pool when the Node.js process terminates
+process.once("SIGTERM", closePoolAndExit).once("SIGINT", closePoolAndExit);
+
 export default pool;
+
+// this function was merged to pool connection test.
+// (async () => {
+//   try {
+//     const { rows } = await pool.query("SELECT current_user");
+//     const currentUser = rows[0]["current_user"];
+//     console.log("Current user:", currentUser);
+//   } catch (err) {
+//     console.error("Error executing query", err);
+//   }
+// })();
