d6c6ebaf6ac35822b4e48cf275a05e3765361f23
jamesedra <dedralin@students.cs.ubc.ca>
2024-03-29 01:28:28 -0700

    bug fix

---
+++ src/backend/controllers/notificationController.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {
+  fetchOwnerWalkTask,
+  fetchFromDB,
+} from "../services/notificationService.js";
+
+const router = express.Router();
+
+/*
+ * NOTE: This notification controller will include its subclasses.
+ *       (FriendPost and WalkAlert)
+ */
+router.get("/", async (req, res) => {
+  const tableContent = await fetchFromDB();
+  res.json({ data: tableContent });
+});
+
+// for side scheduling bar. Fetches all walks tasks based on owner
+router.get("/walk-task/:ownerID", async (req, res) => {
+  try {
+    const ownerID = req.params.ownerID;
+    const tableContent = await fetchOwnerWalkTask(ownerID);
+    if (!tableContent) {
+      res.status(404).json({ error: "WalkTasks not found" });
+      return;
+    }
+    res.json({ data: tableContent });
+  } catch (err) {
+    console.error("Error retrieving walktasks:", err);
+    res.status(500).json({ err: "Internal server error" });
+  }
+});
+
+export default router;
