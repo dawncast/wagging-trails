0f9f8b19099bd10489770f61af3c22f86626a68c
jamesedra <dedralin@students.cs.ubc.ca>
2024-04-04 03:01:18 -0700

    delete schedule done

--- src/frontend/src/components/SchedulingBar/PreviousWalks.js
+++ src/frontend/src/components/SchedulingBar/PreviousWalks.js
@@ -83,7 +83,6 @@ export default function PreviousWalks() {
               </a>
             ) : (
               <button
-                button
                 onClick={() => {
                   setWalk(walk);
                   setShowForm(true);
